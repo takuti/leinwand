f0ace56021b451e01a14a0dee7efb46310331e9a
takuti <k.takuti@gmail.com>
2015-11-24 09:13:48 +0900

    add Window menu (close/minimize window)

--- src/js/editor.js
+++ src/js/editor.js
@@ -195,6 +195,22 @@ Menu.setApplicationMenu(
             click: playSlideshow
           }
         ]
+      },
+      {
+        label: 'Window',
+        role: 'window',
+        submenu: [
+          {
+            label: 'Minimize',
+            accelerator: 'CmdOrCtrl+M',
+            role: 'minimize'
+          },
+          {
+            label: 'Close',
+            accelerator: 'CmdOrCtrl+W',
+            role: 'close'
+          },
+        ]
       }
     ]
   )
